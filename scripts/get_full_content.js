a1f804540b3570b830dba689e794c55f967a064a
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-05-23 14:36:14 +0800

    抽取出 FT 中文网的全文

--- scripts/get_full_content.js
+++ scripts/get_full_content.js
@@ -18,7 +18,8 @@ var headers = {
  * 返回 Promise(content)
  */
 function get_full_content (guid) {
-    uri = guid.slice(0, -5).concat('_all.html')
+    console.log("\n\n =============================================================== \n\n")
+    var uri = full_content_uri(guid)
 
     return new Promise((resolve, reject) => {
 
@@ -30,18 +31,65 @@ function get_full_content (guid) {
         }, function(err, res, body) {
             if(!err && res.statusCode == 200){
                 // 解析返回的 html
-                html = iconv.decode(body, 'gb2312')
-                $ = cheerio.load(html, {decodeEntities: false})
-                
-                let content = $('#endText').html().trim()
+                console.log("获取全文 uri = ", uri)
+                let content = pick_content(guid, body)
                 resolve(content)
             } else {
-                let errReason = 'reject full content request, guid: ' + guid + ' err: ' + err
+                let errReason = 'reject full content request, uri: ' + uri + ' err: ' + err
                 reject(errReason)
             }
         })
     })
-
 }
+
+function ft_or_163(guid) {
+    if(guid.slice(0, 25) == 'http://www.ftchinese.com/')
+        return 'ft'
+    else if(guid.slice(0, 20) == 'http://news.163.com/')
+        return '163'
+}
+
+function full_content_uri(guid) {
+    let uri
+    switch (ft_or_163(guid)) {
+        case 'ft':
+            uri = guid.concat('?full=y')
+            break
+        case '163':
+            uri = guid.slice(0, -5).concat('_all.html')
+            break
+        default:
+            console.log('guid error')
+            break
+    }
+    return uri
+}
+
+function pick_content(guid, body) {
+    let content
+    switch (ft_or_163(guid)) {
+        case 'ft':
+            $ = cheerio.load(body, {decodeEntities: false})
+            content = $('#story-body-container').html().trim()
+            break
+        case '163':
+            html = iconv.decode(body, 'gb2312')
+            $ = cheerio.load(html, {decodeEntities: false})
+            content = $('#endText').html().trim()
+            break
+        default:
+            console.log('pick content error')
+            break
+    }
+    return content
+}
+
+var guid = 
+"http://news.163.com/14/1203/13/ACHV7S6C00014JB6.html"
+// "http://www.ftchinese.com/story/001077660"
+
+get_full_content(guid).then((content)=>{
+    console.log("全文: ", content)
+})
 
 module.exports = get_full_content
