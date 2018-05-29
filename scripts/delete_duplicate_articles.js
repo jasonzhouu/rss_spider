3fcbf042bd5854a11efe41627f6d8dc1c9d7bb70
jasonzhouu <jasonzhouu@163.com>
2018-05-29 10:27:58 +0800

    删除重复文章

--- scripts/delete_duplicate_articles.js
+++ scripts/delete_duplicate_articles.js
@@ -7,12 +7,17 @@ connection.query('select id, guid from articles', (err, res)=>{
         res_array.push(res[key])
     }
 
-    // console.log(res_array.length)
-
     res_array.forEach((value, index)=>{
-        console.log(
-            res_array.slice(index+1).filter(value2 => value2.guid == value.guid).length
-        )
+        let duplicate_article = res_array.slice(index+1).filter(value2 => value2.guid == value.guid)
+        if(duplicate_article.length != 0) {
+            console.log(duplicate_article[0].guid)
+            duplicate_article.forEach((value, index)=>{
+                connection.query('delelte from articles where id=?', value.id, (err, res)=>{
+                    //console.log(err, res)
+                    console.log('删除重复文章 id = ', value.id)
+                })
+            })
+        }
     })
 })
 
